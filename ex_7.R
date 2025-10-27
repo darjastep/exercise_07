a34ce5674ffbf6c6609944ce9bdf35b8e74c615e
Darja Stepankova <247309@vutbr.cz>
2025-10-27 11:00:31 +0100

    vse bez task 6+chybi kontroly

--- ex_7.R
+++ ex_7.R
@@ -40,6 +40,80 @@ Score <- function(start_ind, DNA_string, motif_length) {
   }
   consensus_string <- paste0(consensus, collapse = "")
   
-  return(consensus_string)
+  return(list(consensus_string, score))
 }
+
+seqs <- readDNAStringSet("seq_score.fasta")
+start_ind <- c(15,5,10,2,4) # pocet start ind musi odpovidat poctu sekvenci!!!!
+Score(start_ind, seqs, 6)
+
+## task 2
+NextLeaf <- function(s, t, k ){ 
+  for (i in t:1) {          # loop od posledni dna seq (t je pocet sekvenci)
+    if (s[i] < k) {         # muzem dat uz inkrement?? 
+      s[i] <- s[i] + 1
+      return(s)             
+    }
+    s[i] <- 1
+  }
+  return(s)
+}
+k<- 70 - 5
+NextLeaf(start_ind, 5, k)
+
+## task 3
+BFMotifSearch <- function(DNA, t, n, l){
+  s <- rep(1,t) #1 se opakuje t-krat
+  bestScore <- Score(s, DNA, l)$score # vytahne primo score, mam tam i ten consensus string 
+  bestMotif <- s #?? nevim
+  repeat {
+    
+    s <- NextLeaf(s, t, n - l + 1)
+    
+    # Compute score for current s
+    currentScore <- Score(s, DNA, l)$score
+    
+    # update skore pokud je to potreba
+    if (currentScore > bestScore) {
+      bestScore <- currentScore
+      bestMotif <- s
+    }
+    
+    # stop pokud jsou to zas jen jednicky
+    if (all(s == 1)) {
+      break
+    }
+  }
+  return(bestMotif)
+}
+
+## task 4
+NextVertex <- function(s, i, t, k) {
+  #i = level of vertex, t= num of sequences
+  if (i < t) {
+    s[i+1] <- 1
+    return(list(s = s, i = i + 1))
+  } else { #inkrement ze zadu
+    for (j in t:1) {
+      if (s[j] < k) {
+        s[j] <- s[j] + 1
+        return(list(s = s, i = j))
+        }
+      }
+      return(list(s = s, i = 0))
+  }
+}
+
+## task 5
+ByPass <- function(s, i, t, k) {
+  for (j in i:1) {
+    if (s[j] < k){
+      s[j] <- s[j] + 1
+      return(list(s =s , i=j))
+    }
+  }
+  return(list(s = s, i = 0))
+}
+
+## task 6
 
